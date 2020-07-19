f7b1e9555236a568b8eb00da59bebcab3b7b9e34
can <canazizoglu97@gmail.com>
2020-07-20 03:59:25 +0900

    basic translate helper which is only shows translateable strings in file

---
+++ TranslateHelper/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TranslateHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.sort = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.a = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.b = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.sort,
+            this.a,
+            this.b});
+            this.dataGridView1.Location = new System.Drawing.Point(147, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(682, 328);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // sort
+            // 
+            this.sort.HeaderText = "Sort";
+            this.sort.Name = "sort";
+            // 
+            // a
+            // 
+            this.a.FillWeight = 400F;
+            this.a.HeaderText = "Base String";
+            this.a.Name = "a";
+            this.a.Width = 400;
+            // 
+            // b
+            // 
+            this.b.HeaderText = "Target";
+            this.b.Name = "b";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 352);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sort;
+        private System.Windows.Forms.DataGridViewTextBoxColumn a;
+        private System.Windows.Forms.DataGridViewTextBoxColumn b;
+    }
+}
+
---
+++ TranslateHelper/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TranslateHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog op = new OpenFileDialog();
+            if(op.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.Rows.Clear();
+                string result = op.FileName;
+                FileStream f = File.Open(result, FileMode.Open, FileAccess.Read);
+                int fsize = (int)f.Length;
+                byte[] content = new byte[fsize];
+                f.Read(content, 0, fsize);
+                f.Close();
+
+                int labelOffset = 0;
+                int blockSize = 4096;
+                int labelCount = fsize / blockSize;
+
+                //every string has 4096 byte space so i can pre calculate count of total string
+                //and iterate it
+                for (int i = 0; i < labelCount; i++)
+                {
+                    //i should find the place of string in block so i check until i hit
+                    //something different than 0x00
+                        int limit = blockSize * (i + 1);
+                        labelOffset = blockSize * i;
+                
+                        while (content[labelOffset] == 0x00)
+                        {
+                            labelOffset++;
+                        }
+                        //if i reach to end or exceed it that means there is no string in block
+                        if (labelOffset >= limit)
+                        { 
+                            dataGridView1.Rows.Add(0, "might be important for data health");
+                            continue;
+                        }
+                    //once i found the string i can extract the string for translate
+                        
+
+                        int sort = content[labelOffset + 1] >> 8 | content[labelOffset] << 8;
+                        labelOffset += 2;
+                        List<byte> label = new List<byte>();
+                        while (content[labelOffset] != 0x00)
+                        {
+                            label.Add(content[labelOffset]);
+                            labelOffset++;
+                        }
+                        dataGridView1.Rows.Add(sort,Encoding.Default.GetString(label.ToArray()));
+                       
+
+                }
+
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
